c20f1c624df79e01a987d21cb30314209ed4ba98
viniciusscampos <silvcamposvinicius@gmail.com>
2016-07-23 17:48:54 -0300

    Exercicio 5 incompleto.

--- TP3/Exercicio5/xmatrix.c
+++ TP3/Exercicio5/xmatrix.c
@@ -24,40 +24,42 @@ float** generateRandomMatrix() {
 int main (int argc, char** argv)
 {
 	MPI_Init(&argc, &argv);
-	int rank, nproc,sendcount,recvcount,source;	
+	int rank, nproc;	
 	MPI_Request request;
 	MPI_Status status;
+	MPI_Datatype rowtype,coltype;
 	unsigned long count = (unsigned long) N * (unsigned long)N;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	
+	float recvbufA[N];
+	float recvbufB[N];
+	float **product;
 
 	if(rank==0){		
-		float **A = generateRandomMatrix();
-		float **B = generateRandomMatrix();
-		
-		float recvbufA[N];
-		float recvbufB[N];
-		MPI_Scatter(&(A[0][0]),N,MPI_FLOAT,recvbufA,N,MPI_FLOAT,rank,MPI_COMM_WORLD);
-		MPI_Scatter(&(B[0][0]),N,MPI_FLOAT,recvbufB,N,MPI_FLOAT,rank,MPI_COMM_WORLD);		
-		/*
-		for(int i=0;i<N;i++){
-			for(int j=0;j<N;j++){
-				printf("%f\n",A[i][j] );
-			}
-		}
-		*/
-	}
+		float **A = {{1,1},{1,1}};
+		float **B = {{1,1},{1,1}};
+		product = (float *)malloc(sizeof(float)*count);
 
-	float *C;
-	C = malloc(N*sizeof(float));
-	for(int i=0;i<N;i++){
-		C[i] = rec
+		MPI_Type_vector(N,1,MPI_FLOAT,N,&coltype);
+		MPI_Type_commit(&rowtype);
+
+		MPI_Scatter(&(A[0][0]),N,MPI_FLOAT,recvbufA,N,MPI_FLOAT,rank,MPI_COMM_WORLD);		
+		for(int i=0;i<nproc;i++){
+			MPI_Send(&B[0][i],1,coltype,i,i,MPI_COMM_WORLD);		
+		}		
 	}
 	
+	MPI_Recv(recvbufB,N,MPI_FLOAT,0,0,MPI_COMM_WORLD,&status);
+	
+	float *C = malloc(N*sizeof(float));
+	for(int i=0;i<N;i++){
+		C[i] = recvbufA[i]*recvbufB[i];
+	}
+	
+	MPI_Gather(C,N,MPI_FLOAT,product,N,MPI_FLOAT,0,MPI_COMM_WORLD);
 
 		
-
 	/*
 	MPI_Isend(&(A[0][0]),count,MPI_FLOAT,right,0,MPI_COMM_WORLD,&request);
 	MPI_Irecv(&(B[0][0]),count,MPI_FLOAT,left,0,MPI_COMM_WORLD,&request);
