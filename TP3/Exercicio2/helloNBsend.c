9903614ac190decf297bf3a1413f9610bc504ed0
viniciusscampos <silvcamposvinicius@gmail.com>
2016-07-22 21:37:54 -0300

    HelloNBSend.

---
+++ TP3/Exercicio2/helloNBsend.c
@@ -0,0 +1,36 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main (argc, argv)
+int argc;
+char **argv;
+{
+	MPI_Init(&argc, &argv);
+	int rank, nproc, partner,buffer;
+	MPI_Request request;
+	MPI_Status status;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+	printf("Hello world! Eu sou o processo %d.\n", rank);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(rank==0){
+		printf("O número total de tarefas é: %d\n", nproc);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank < nproc/2){
+		partner = nproc/2 + rank;
+	}
+	else if (rank >= nproc/2){
+		partner = rank - nproc/2;
+	}
+
+	buffer = rank;	
+	MPI_Isend(&buffer,1,MPI_INT,partner,0,MPI_COMM_WORLD,&request);
+	MPI_Irecv(&buffer,1,MPI_INT,partner,0,MPI_COMM_WORLD,&request);
+
+	MPI_Wait(&request,&status);	
+	printf("A tarefa %d tem parceira %d.\n", rank,partner);	
+
+	MPI_Finalize();
+	return 0;
+}
